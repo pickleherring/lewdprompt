11cb72642f3e386d8d4fd2327ffe8f0a23fb060f
pickleherring <100018622+pickleherring@users.noreply.github.com>
2022-06-23 11:40:19 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+"""The lewdprompt app.
+"""
+
+import random
+
+import streamlit
+
+
+adjectives = [
+    'buff',
+    'busty',
+    'horny',
+    'hung',
+    'ravenous',
+    'shy',
+]
+
+modifiers = [
+    'big-butt',
+    'college',
+    'horsecock',
+    'lactating',
+    'lesbian',
+    'pregnant',
+    'virgin',
+]
+
+nouns = [
+    'amazons',
+    'bimbos',
+    'catgirls',
+    'centaurs',
+    'cowgirls',
+    'demons',
+    'nuns',
+    'vampires',
+    'witches',
+]
+
+random.shuffle(adjectives)
+random.shuffle(modifiers)
+random.shuffle(nouns)
+
+
+def get_phrase():
+
+    adjective = adjectives.pop()
+    modifier = modifiers.pop()
+    noun = nouns.pop()
+
+    return ' '.join([adjective, modifier, noun])
+
+
+streamlit.title('generate frivolous prompts for hentai stories')
+
+streamlit.markdown('''
+This app is about S E C K S. By clicking the button below, you declare that you are an adult and consent to reading lewd words.
+''')
+
+if streamlit.button('I am an adult, give me a prompt!'):
+
+    phrase1 = get_phrase()
+    phrase2 = get_phrase()
+
+    text = f'{phrase1} vs. {phrase2}!'
+
+    streamlit.subheader(text.capitalize())
